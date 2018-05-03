2645875bc9f9c8356e9564e00345e3c24e471be3
root <root@ip-172-31-86-0.ec2.internal>
2018-05-03 04:52:27 +0000

    Added from SSSathya

---
+++ demo.java
@@ -0,0 +1,24 @@
+class Demo
+{
+  void SUB()
+  {
+     a-b
+  } 
+  void ADD()
+  {
+     a+b
+  } 
+  void DEPOSITE()
+  {
+    .......
+  }
+  void WITHDRAW()
+  {
+    ....... 
+  }
+  p s void main()
+  {
+    .....
+    s.o.p("hello ");
+  } 
+}
